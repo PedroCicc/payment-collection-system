e7077f8486501745c7bbf7b7804ebdf1d3298339
Pedro Ciccone <pedrocicc14@gmail.com>
2020-11-15 15:48:17 -0300

    Commit: more initial files.

---
+++ index.js
@@ -0,0 +1,11 @@
+const Koa = require("koa");
+const bodyparser = require("koa-bodyparser");
+const router = require("./src/routes");
+
+const server = new Koa();
+
+const PORT = process.env.PORT || 8081;
+server.use(bodyparser());
+server.use(router.routes());
+
+server.listen(PORT, () => console.log("Servidor rodando na porta: ", PORT));
