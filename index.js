3fe0283f1146bb2a66173e2f4c29ef18cc7b5506
Pedro Ciccone <pedrocicc14@gmail.com>
2020-11-24 19:25:58 -0300

    feat: created index.js.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const Koa = require("koa");
-const bodyparser = require("koa-bodyparser");
-const router = require("./src/routes");
+const Koa = require('koa');
+const bodyparser = require('koa-bodyparser');
+const router = require('./src/routes');
 
 const server = new Koa();
 
@@ -8,4 +8,4 @@ const PORT = process.env.PORT || 8081;
 server.use(bodyparser());
 server.use(router.routes());
 
-server.listen(PORT, () => console.log("Servidor rodando na porta: ", PORT));
+server.listen(PORT, () => console.log('Servidor rodando na porta: ', PORT));
