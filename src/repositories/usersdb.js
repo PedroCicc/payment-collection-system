07c62acf435fdd5b91adcf64037cf36986b56b89
Pedro Ciccone <pedrocicc14@gmail.com>
2020-11-24 19:36:25 -0300

    feat: created usersdb.js, with the getUserByEmail and insertUser functions.

---
+++ src/repositories/usersdb.js
@@ -0,0 +1,31 @@
+const db = require('../utils/database');
+
+async function getUserByEmail(email) {
+	const query = 'SELECT * FROM users WHERE email = $1';
+
+	const result = await db.query({
+		text: query,
+		values: [email],
+	});
+
+	return result.rows.shift();
+}
+
+async function insertUser(email, password) {
+	const query = `INSERT INTO users (
+		email, password
+		) values ($1, $2)
+		RETURNING *`;
+
+	const result = await db.query({
+		text: query,
+		values: [email, password],
+	});
+
+	return result.rows.shift();
+}
+
+module.exports = {
+	getUserByEmail,
+	insertUser,
+};
