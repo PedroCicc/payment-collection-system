6d15d64e84feb0d31eb64c4d9d777724244454f3
Pedro Ciccone <pedrocicc14@gmail.com>
2020-11-24 19:31:33 -0300

    feat: created auth.js to implement the login function.

---
+++ src/controllers/auth.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+const response = require('./response');
+const UsersDB = require('../repositories/usersdb');
+const Password = require('../utils/password');
+const users = require('../repositories/usersdb');
+
+require('dotenv').config();
+
+async function authenticate(ctx) {
+	const { email = null, password = null } = ctx.request.body;
+
+	if (!email || !password) {
+		return response(ctx, 400, { message: 'Pedido mal formatado.' });
+	}
+
+	const user = await UsersDB.getUserByEmail(email);
+
+	if (user) {
+		const comparison = await Password.check(password, user.password);
+		if (comparison) {
+			const token = jwt.sign(
+				{ id: users.id, email: users.email },
+				process.env.JWT_SECRET,
+				{
+					expiresIn: '1h',
+				}
+			);
+			return response(ctx, 200, {
+				message: 'Usuário logado com sucesso!',
+				token,
+			});
+		}
+	}
+	return response(ctx, 400, { message: 'Email ou senha incorretos.' });
+}
+
+module.exports = { authenticate };
