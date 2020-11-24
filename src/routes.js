550344d7c975054019e82f76971ac7af67457f0f
Pedro Ciccone <pedrocicc14@gmail.com>
2020-11-24 19:27:26 -0300

    feat: created routes.js and the estructure of 4 endpoints.

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,16 @@
-const Router = require("koa-router");
+const Router = require('koa-router');
 
 const router = new Router();
+
+const Auth = require('./controllers/auth');
+const Users = require('./controllers/users');
+const Clients = require('./controllers/clients');
+
+router.post('/auth', Auth.authenticate());
+router.post('/usuarios', Users.createUser());
+router.post('/clientes', Clients.createClient());
+router.put('/clientes', Clients.editClient());
+
+router.get('/cobrancas', Payments.createPayment());
 
 module.exports = router;
