c7b826b86b39d3c468128f84f893f66b0e09c35e
Pedro Ciccone <pedrocicc14@gmail.com>
2020-11-24 21:09:26 -0300

    fix(routes): fixed some errors at every endpoint, where I was calling each function instead of just mentioning them.

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,12 @@ const router = new Router();
 const Auth = require('./controllers/auth');
 const Users = require('./controllers/users');
 const Clients = require('./controllers/clients');
+const Payments = require('./controllers/payments');
 
-router.post('/auth', Auth.authenticate());
-router.post('/usuarios', Users.createUser());
-router.post('/clientes', Clients.createClient());
-router.put('/clientes', Clients.editClient());
-
-router.get('/cobrancas', Payments.createPayment());
+router.post('/auth', Auth.authenticate);
+router.post('/usuarios', Users.createUser);
+router.post('/clientes', Clients.createClient);
+router.put('/clientes', Clients.editClient);
+router.post('/cobrancas', Payments.createPayment);
 
 module.exports = router;
