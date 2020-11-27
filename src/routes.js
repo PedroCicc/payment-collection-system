f9fbba4965495da2a4908c187efc9e14dec6c0e9
Pedro Ciccone <pedrocicc14@gmail.com>
2020-11-26 22:46:26 -0300

    feat(routes): added the middlewares to routes.js.

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const Router = require('koa-router');
 
 const router = new Router();
+const Encrypt = require('./middlewares/encrypt');
+const Session = require('./middlewares/session');
 
 const Auth = require('./controllers/auth');
 const Users = require('./controllers/users');
@@ -8,9 +10,9 @@ const Clients = require('./controllers/clients');
 const Payments = require('./controllers/payments');
 
 router.post('/auth', Auth.authenticate);
-router.post('/usuarios', Users.createUser);
-router.post('/clientes', Clients.createClient);
-router.put('/clientes', Clients.editClient);
-router.post('/cobrancas', Payments.createPayment);
+router.post('/usuarios', Encrypt.encrypt, Users.createUser);
+router.post('/clientes', Session.verify, Clients.createClient);
+// router.put('/clientes', Clients.editClient);
+router.post('/cobrancas', Session.verify, Payments.createPayment);
 
 module.exports = router;
