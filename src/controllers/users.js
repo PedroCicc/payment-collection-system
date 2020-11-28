69c97d24636dbbd3b68d7ae15c1b242387fbdef3
Pedro Ciccone <pedrocicc14@gmail.com>
2020-11-28 18:41:27 -0300

    fix(users): fixed some variable names.

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,27 +2,27 @@ const response = require('./response');
 const UsersDB = require('../repositories/usersdb');
 
 async function createUser(ctx) {
-	const { email = null } = ctx.request.body;
-	console.log(email);
-	if (!email) {
-		return response(ctx, 400, { message: 'Pedido mal formatado.' });
+	const { email = null, nome = null } = ctx.request.body;
+
+	if (!email || !nome) {
+		return response(ctx, 400, { mensagem: 'Pedido mal formatado.' });
 	}
 
 	const user = await UsersDB.getUserByEmail(email);
 
 	if (user) {
 		return response(ctx, 400, {
-			message: 'Esse usuário já existe no nosso banco de dados.',
+			mensagem: 'Esse usuário já existe no nosso banco de dados.',
 		});
 	}
 
-	const newUser = await UsersDB.insertUser(email, ctx.state.hash);
+	const newUser = await UsersDB.insertUser(email, ctx.state.hash, nome);
 
 	if (newUser) {
 		return response(ctx, 201, { id: newUser.id });
 	}
 
-	return response(ctx, 400, { message: 'Erro desconhecido.' });
+	return response(ctx, 400, { mensagem: 'Erro desconhecido.' });
 }
 
 module.exports = { createUser };
