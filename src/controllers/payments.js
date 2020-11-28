7e6e079fef598fb9a0b9e60021917b1415a8730a
Pedro Ciccone <pedrocicc14@gmail.com>
2020-11-28 18:35:43 -0300

    feat(payments): started the integration with Pagar.me.

--- src/controllers/payments.js
+++ src/controllers/payments.js
@@ -1,48 +1,61 @@
 const response = require('./response');
+const ClientsDB = require('../repositories/clientsdb');
 const PaymentsDB = require('../repositories/paymentsdb');
+const Pagarme = require('../utils/pagarme');
 
 async function createPayment(ctx) {
 	const {
-		clientId = null,
-		description = null,
-		value = null,
-		dueDate = null,
+		idDoCliente = null,
+		descricao = null,
+		valor = null,
+		vencimento = null,
 	} = ctx.request.body;
 
-	if (!clientId || !description || !value || !dueDate) {
-		return response(ctx, 400, { message: 'Pedido mal formatado.' });
+	const userId = ctx.state.id;
+
+	if (!idDoCliente || !descricao || !valor || !vencimento) {
+		return response(ctx, 400, { mensagem: 'Pedido mal formatado.' });
 	}
 
-	// Corrigir, não posso checar se a cobrança existe pelo client id
-	const payment = await PaymentsDB.getPaymentsByClientId(clientId);
+	const cliente = await ClientsDB.getClientById(userId, idDoCliente);
 
-	if (payment) {
-		return response(ctx, 400, {
-			message: 'Essa cobrança já foi criada no nosso banco de dados.',
-		});
+	if (!cliente) {
+		return response(ctx, 400, { mensagem: 'O cliente não existe.' });
 	}
 
-	const newPayment = await PaymentsDB.insertPayment(
-		clientId,
-		description,
-		value,
-		dueDate
+	const boletoBancario = await Pagarme.gerarBoleto(
+		cliente.name,
+		cliente.cpf,
+		descricao,
+		valor,
+		vencimento
 	);
+
+	if (!boletoBancario) {
+		return response(ctx, 400, {
+			mensagem: 'Não foi possível gerar o boleto.',
+		});
+		// Exibir erro da pagarme
+	}
+
+	const newPayment = await PaymentsDB.insertPayment(boletoBancario);
 
 	if (newPayment) {
 		return response(ctx, 201, {
-			payment: {
-				clientId: newPayment.clientId,
-				description: newPayment.description,
-				value: newPayment.value,
-				dueDate: newPayment.dueDate,
-				paymentSlipLink: paymentSlipLink,
+			cobranca: {
+				idDoCliente: newPayment.idDoCliente,
+				descricao: newPayment.descricao,
+				valor: newPayment.valor,
+				vencimento: newPayment.vencimento,
+				linkDoBoleto: transaction.boletoUrl,
 				status: 'AGUARDANDO',
 			},
 		});
 	}
 
-	return response(ctx, 400, { message: 'Erro desconhecido.' });
+	// return response(ctx, 200, { mensagem: 'Sucesso.', boletoBancario });
+
+	return response(ctx, 400, { mensagem: 'Erro desconhecido.' });
 }
 
 module.exports = {
