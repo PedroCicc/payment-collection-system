a06f980ee02d2adf9dbf82af0f40e9f32dcd43f8
Pedro Ciccone <pedrocicc14@gmail.com>
2020-11-28 18:50:51 -0300

    fix(auth): deleted some unnecessary logs.

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -6,12 +6,9 @@ require('dotenv').config();
 async function verify(ctx, next) {
 	try {
 		const [bearer, token] = ctx.headers.authorization.split(' ');
-		console.log(process.env.JWT_SECRET);
 		const verification = await jwt.verify(token, process.env.JWT_SECRET);
-		console.log(verification);
 		ctx.state.id = verification.id;
 		ctx.state.email = verification.email;
-		console.log(`session: ${ctx.state.id}`);
 	} catch (err) {
 		return response(ctx, 403, 'Ação proibida');
 	}
