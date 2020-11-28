7a3a9ae79e959d0055a281af29d3840b75be1c8a
Pedro Ciccone <pedrocicc14@gmail.com>
2020-11-28 18:57:44 -0300

    feat(clients): added getClientById, editClient and getClients functions.

--- src/repositories/clientsdb.js
+++ src/repositories/clientsdb.js
@@ -1,8 +1,9 @@
+const { off } = require('../utils/database');
 const db = require('../utils/database');
 
 async function getClientByEmail(email) {
 	const query = 'SELECT * FROM clients WHERE email = $1';
-
+	// Pegar userID
 	const result = await db.query({
 		text: query,
 		values: [email],
@@ -11,7 +12,18 @@ async function getClientByEmail(email) {
 	return result.rows.shift();
 }
 
-async function insertClient(name, cpf, email, tel, userId) {
+async function getClientById(userId, idDoCliente) {
+	const query = 'SELECT * FROM clients WHERE user_id = $1 AND id = $2';
+
+	const result = await db.query({
+		text: query,
+		values: [userId, idDoCliente],
+	});
+
+	return result.rows.shift();
+}
+
+async function insertClient(nome, cpf, email, tel, idDoUsuario) {
 	const query = `INSERT INTO clients (
 		name, cpf, email, tel, user_id
 		) values ($1, $2, $3, $4, $5)
@@ -19,13 +31,42 @@ async function insertClient(name, cpf, email, tel, userId) {
 
 	const result = await db.query({
 		text: query,
-		values: [name, cpf, email, tel, userId],
+		values: [nome, cpf, email, tel, idDoUsuario],
 	});
 
 	return result.rows.shift();
 }
 
+async function editClient(id, nome, cpf, email) {
+	const query = `UPDATE clients SET
+	name = $2, cpf = $3, email = $4
+	WHERE id = $1 RETURNING *`;
+
+	const result = await db.query({
+		text: query,
+		values: [id, nome, cpf, email],
+	});
+
+	return result.rows.shift();
+}
+
+async function getClients(userId, clientesPorPagina, offset) {
+	const query = `SELECT * FROM clients WHERE user_id = $1 LIMIT $2 OFFSET $3`;
+
+	const result = await db.query({
+		text: query,
+		values: [userId, clientesPorPagina, offset],
+	});
+
+	return result.rows;
+}
+
+// Formular função searchClients
+
 module.exports = {
 	getClientByEmail,
+	getClientById,
 	insertClient,
+	editClient,
+	getClients,
 };
