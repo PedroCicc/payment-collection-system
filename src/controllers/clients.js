aedc6776a029fa3b0eff9fbf55f7a96e9618ea91
Pedro Ciccone <pedrocicc14@gmail.com>
2020-11-28 18:30:26 -0300

    feat(client): added the editClient function.

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -1,18 +1,19 @@
 const response = require('./response');
 const ClientsDB = require('../repositories/clientsdb');
+const { off } = require('../utils/database');
 
 async function createClient(ctx) {
 	const {
-		name = null,
+		nome = null,
 		cpf = null,
 		email = null,
 		tel = null,
 	} = ctx.request.body;
 
-	const userId = ctx.state.id;
+	const idDoUsuario = ctx.state.id;
 
-	if (!name || !cpf || !email || !tel) {
-		return response(ctx, 400, { message: 'Pedido mal formatado.' });
+	if (!nome || !cpf || !email || !tel) {
+		return response(ctx, 400, { mensagem: 'Pedido mal formatado.' });
 	}
 
 	const client = await ClientsDB.getClientByEmail(email);
@@ -24,36 +25,94 @@ async function createClient(ctx) {
 	}
 
 	const newClient = await ClientsDB.insertClient(
-		name,
+		nome,
 		cpf,
 		email,
 		tel,
-		userId
+		idDoUsuario
 	);
 
 	if (newClient) {
 		return response(ctx, 201, { id: newClient.id });
 	}
 
-	return response(ctx, 400, { message: 'Erro desconhecido.' });
+	return response(ctx, 400, { mensagem: 'Erro desconhecido.' });
 }
 
-// const getClients = (ctx, userId) => {
-// 	const { clientsPerPage = null, offset = null } = ctx.query;
+async function editClient(ctx) {
+	const {
+		id = null,
+		nome = null,
+		cpf = null,
+		email = null,
+	} = ctx.request.body;
 
-// 	if (!clientsPerPage)
-// }
+	if (!id || !nome || !cpf || !email) {
+		return response(ctx, 400, { mensagem: 'Pedido mal formatado.' });
+	}
 
-// async function editClient(ctx) {
-// const {
-// name = null,
-// cpf = null,
-// email = null,
-// tel = null,
-// } = ctx.request.body;
-// }
+	const result = await ClientsDB.editClient(id, nome, cpf, email);
+
+	if (result) {
+		return response(ctx, 200, {
+			data: {
+				id: result.id,
+				nome: result.nome,
+				cpf: result.cpf,
+				email: result.email,
+			},
+		});
+	}
+
+	return response(ctx, 400, {
+		mensagem: 'Nenhum cliente encontrado com esse id.',
+	});
+}
+
+async function getClients(ctx) {
+	const userId = ctx.state.id;
+	const { busca = null, clientesPorPagina = 10, offset = 0 } = ctx.query;
+
+	console.log(userId);
+	if (!userId) {
+		return response(ctx, 400, { mensagem: 'Pedido mal formatado.' });
+	}
+
+	if (busca) {
+		const result = await ClientsDB.searchClients(
+			userId,
+			busca,
+			clientesPorPagina,
+			offset
+		);
+
+		if (result) {
+			return response(ctx, 200, result);
+		}
+
+		return response(ctx, 400, {
+			mensagem: 'Nenhum usuário encontrado com esse id.',
+		});
+		// eslint-disable-next-line no-else-return
+	} else {
+		const result = await ClientsDB.getClients(
+			userId,
+			clientesPorPagina,
+			offset
+		);
+
+		if (result) {
+			return response(ctx, 200, result);
+		}
+
+		return response(ctx, 400, {
+			mensagem: 'Nenhum usuário encontrado com esse id.',
+		});
+	}
+}
 
 module.exports = {
 	createClient,
-	// getClients,
+	editClient,
+	getClients,
 };
