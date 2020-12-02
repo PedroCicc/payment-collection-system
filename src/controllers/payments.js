850a7694a03a7ab4eb27a2302907db8fde5f39a0
Pedro Ciccone <pedrocicc14@gmail.com>
2020-12-01 22:53:17 -0300

    feat(payments): fixed the createPayment response and added the payPayment function.

--- src/controllers/payments.js
+++ src/controllers/payments.js
@@ -30,34 +30,52 @@ async function createPayment(ctx) {
 		valor,
 		vencimento
 	);
-
+	// console.log(boletoBancario);
 	if (!boletoBancario) {
 		return response(ctx, 400, {
 			mensagem: 'Não foi possível gerar o boleto.',
 		});
 		// Exibir erro da pagarme
 	}
-
-	const newPayment = await PaymentsDB.insertPayment(boletoBancario);
+	
+	const newPayment = await PaymentsDB.insertPayment(idDoCliente, descricao, valor, vencimento, boletoBancario.boleto_url, boletoBancario.boleto_barcode, boletoBancario.status);
+	// console.log(newPayment);
 
 	if (newPayment) {
 		return response(ctx, 201, {
 			cobranca: {
-				idDoCliente: newPayment.idDoCliente,
-				descricao: newPayment.descricao,
-				valor: newPayment.valor,
-				vencimento: newPayment.vencimento,
-				linkDoBoleto: transaction.boletoUrl,
-				status: 'AGUARDANDO',
+				idDoCliente,
+				descricao,
+				valor,
+				vencimento,
+				linkDoBoleto: boletoBancario.boleto_url,
+				status: 'AGUARDANDO'
 			},
 		});
 	}
-
 	// return response(ctx, 200, { mensagem: 'Sucesso.', boletoBancario });
 
 	return response(ctx, 400, { mensagem: 'Erro desconhecido.' });
 }
 
+async function payPayment(ctx) {
+	const { idDaCobranca = null } = ctx.request.body;
+	console.log(idDaCobranca);
+	if (!idDaCobranca) {
+		return response(ctx, 400, { mensagem: 'Pedido mal formatado.' });
+	}
+
+	const payment = await PaymentsDB.getPaymentById(idDaCobranca);
+	console.log(`Payment: ${payment}`);
+	if (!payment) {
+		return response(ctx, 400, { mensagem: 'A cobrança não existe.' });
+	}
+
+	// Payment tá dando undefined, pq?
+	// Como pegar boleto exato que eu quero pagar se não tá indo pelo id dele? Pego id do usuario?
+}
+
 module.exports = {
 	createPayment,
+	payPayment
 };
