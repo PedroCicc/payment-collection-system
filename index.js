553b3445ebf2b93a3058e3ba7add570995794bf5
Pedro Ciccone <pedrocicc14@gmail.com>
2020-12-03 23:52:15 -0300

    doc(index): installed koa-cors.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Koa = require('koa');
+const cors = require('@koa/cors');
 const bodyparser = require('koa-bodyparser');
 const router = require('./src/routes');
 
 const server = new Koa();
+server.use(cors());
 
 const PORT = process.env.PORT || 8081;
 server.use(bodyparser());
