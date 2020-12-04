f345826fc4fd048524b33eecb69995ef22833e5a
Pedro Ciccone <pedrocicc14@gmail.com>
2020-12-03 23:54:34 -0300

    feat(payments): added the setPaymentsAsPaid and getPayments functions.

--- src/repositories/paymentsdb.js
+++ src/repositories/paymentsdb.js
@@ -5,21 +5,54 @@ async function insertPayment(clientId, description, value, dueDate, linkDoBoleto
 		client_id, description, value, due_date, payment_slip_link, codebar
 	) values ($1, $2, $3, $4, $5, $6)
 	RETURNING *`;
-	console.log(clientId, description, value, dueDate, linkDoBoleto, codebar);
+
 	const result = await db.query({
 		text: query,
 		values: [clientId, description, value, dueDate, linkDoBoleto, codebar],
 	});
-	// console.log(result);
+
 	return result.rows.shift();
 }
 
-async function getPaymentById(idDaCobranca) {
-	const query = `SELECT * FROM payments WHERE id = $1`;
+async function setPaymentAsPaid(idDaCobranca, currentDate) {
+	const query = `UPDATE payments SET payment_date = $2
+	WHERE payments.id = $1
+	RETURNING *`;
 
 	const result = await db.query({
 		text: query,
-		values: [idDaCobranca],
+		values: [idDaCobranca, currentDate],
+	});
+
+	return result.rows.shift();
+}
+
+async function getPayments(userId, cobrancasPorPagina, offset) {
+	const query = `SELECT payments.id, payments.client_id, payments.description, payments.value,
+		payments.due_date, payments.payment_date, payments.payment_slip_link
+		FROM payments
+		INNER JOIN clients
+		ON payments.client_id = clients.id
+		WHERE clients.user_id = $1
+		LIMIT $2 OFFSET $3;`;
+
+	const result = await db.query({
+		text: query,
+		values: [userId, cobrancasPorPagina, offset]
+	});
+
+	return result.rows;
+}
+
+async function countPaymentsByPage(userId) {
+	const query = `SELECT count(*)::INTEGER as contagem_de_cobrancas FROM payments
+	INNER JOIN clients
+	ON payments.client_id = clients.id
+	WHERE clients.user_id = $1;`;
+
+	const result = await db.query({
+		text: query,
+		values: [userId]
 	});
 
 	return result.rows.shift();
@@ -27,5 +60,7 @@ async function getPaymentById(idDaCobranca) {
 
 module.exports = {
 	insertPayment,
-	getPaymentById
+	setPaymentAsPaid,
+	getPayments,
+	countPaymentsByPage,
 };
